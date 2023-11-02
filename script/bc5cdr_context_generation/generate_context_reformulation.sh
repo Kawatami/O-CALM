750d3c1898bf7d8065a05d6208dbff018564064c
Tristan Luiggi <luiggi@upmc.isir.fr>
2023-11-02 15:01:21 +0100

    adding context generation script for b5cdr dataset

---
+++ script/bc5cdr_context_generation/generate_context_reformulation.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --partition=hard
+
+#SBATCH --job-name=context_generation
+
+#SBATCH --nodes=1
+
+#SBATCH --time=5-00:00:00
+
+#SBATCH --gpus-per-node=1
+
+#SBATCH --mail-type=ALL
+
+#SBATCH --output=/home/luiggit/project/contextual_ner/script/logs/baseline_large.out
+
+#SBATCH --error=/home/luiggit/project/contextual_ner/script/logs/baseline_large.out
+
+source ~/miniconda3/etc/profile.d/conda.sh
+
+conda activate deep
+
+for N_RUN in 1
+do
+
+
+
+echo ===== RUN $N_RUN ==============
+
+
+python    ../generate_context_dataset.py  \
+          --path ../data/WNUT17/CLNER_datasets/bc5cdr/\
+          --destination ../annot_reformulation_bc5cdr \
+          --use_cuda \
+          --batch_size  2 \
+          --prompts_path ../prompts_reformulation.json \
+          --is_split_into_words
+
+
+done
