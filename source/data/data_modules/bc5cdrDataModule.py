1dc7daa0ea7b420ba978d042fa66aaba4277909e
Tristan Luiggi <luiggi@upmc.isir.fr>
2023-11-06 14:42:20 +0100

    createing base DataModule for datasets and adding bc5dcr DataModule

--- source/data/data_modules/bc5cdrDataModule.py
+++ source/data/data_modules/bc5cdrDataModule.py
@@ -2,41 +2,34 @@ from __future__ import annotations
 from argparse import ArgumentParser, Namespace
 from source.utils.register import register
 from enum import Enum
-from source.utils.misc import load_from_file, list_file
+from source.utils.misc import load_from_file, list_file_bc5cdr
 from source.data.data_modules.NERDataModule import NERDataModule
 
 # defining WNUT17 labels
-class WNUT17_label(Enum):
-    B_corporation = 0
-    B_creative_work = 1
-    B_group = 2
-    B_location = 3
-    B_person = 4
-    B_product = 5
-    I_corporation = 6
-    I_creative_work = 7
-    I_group = 8
-    I_location = 9
-    I_person = 10
-    I_product = 11
-    O = 12
+class BC5CDR_label(Enum):
+    B_Chemical = 0
+    B_Disease = 1
+    I_Chemical = 2
+    I_Disease = 3
+    O = 4
     B_X = -100 # always ignore context token in loss processing
 
 
 @register("DATASETS")
-class WNUT17DataModule(NERDataModule) :
+class Bc5cdrDataModule(NERDataModule) :
 
     def __init__(self, **kwargs) :
 
         super().__init__(
             file_loader = load_from_file,
-            label_enum = WNUT17_label,
+            label_enum = BC5CDR_label,
+            file_detector = list_file_bc5cdr,
             **kwargs
         )
 
 
     @classmethod
-    def from_args(cls, args : Namespace) -> WNUT17DataModule :
+    def from_args(cls, args : Namespace) -> Bc5cdrDataModule :
         """
         Build Rotowire Task 1 Datamodule from parser Namspace args
         :param args: namespace from main parser
